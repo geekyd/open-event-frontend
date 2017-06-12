eade8cd42b2c0ab91baef50eb04b20d728278f72
geekyd <singhsince94@gmail.com>
2017-06-12 23:10:39 +0530

    Adds model & form validation to settings/images

--- tests/integration/components/forms/admin/settings/images-form-test.js
+++ tests/integration/components/forms/admin/settings/images-form-test.js
@@ -2,12 +2,12 @@ import { test } from 'ember-qunit';
 import moduleForComponent from 'open-event-frontend/tests/helpers/component-helper';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('forms/admin/settings/images-form', 'Integration | Component | forms/admin/settings/images form', {
-  integration: true
-});
+moduleForComponent('forms/admin/settings/images-form', 'Integration | Component | forms/admin/settings/images form');
+
+const images = [{ type: 'event', logoWidth: 10 }];
 
 test('it renders', function(assert) {
-  this.render(hbs`{{forms/admin/settings/system-form}}`);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('images', images);
+  this.render(hbs`{{forms/admin/settings/images-form images=images}}`);
+  assert.ok(this.$().html().trim().includes('Event Images'));
 });
